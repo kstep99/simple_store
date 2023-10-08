a56587abf152e676bccbb941eaf0ec2c3ec2b478
kstep99 <kstep99@outlook.com>
2023-10-08 06:07:20 -0500

    finished validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,7 @@
 class Product < ApplicationRecord
+  # ... any existing code ...
+
+  validates :title, presence: true
+  validates :price, presence: true, numericality: true
+  validates :stock_quantity, presence: true, numericality: { only_integer: true }
 end
